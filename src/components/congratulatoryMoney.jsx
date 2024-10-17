c17da005241d0fea6813a4260dce986f217d2f75
shstat1729 <smartsangho94@gmail.com>
2024-10-17 18:49:27 +0900

    Adding

--- src/components/congratulatoryMoney.jsx
+++ src/components/congratulatoryMoney.jsx
@@ -113,7 +113,8 @@ const CongratulatoryMoney = () => {
         onCancel={() => setGroomVisible(false)}
         footer={[
           <Description>
-            계좌번호 클릭시, 붙여넣기 가능한 텍스트로 복사됩니다.
+            계좌번호 클릭시,\n
+            붙여넣기 가능한 텍스트로 복사됩니다.
           </Description>,
         ]}
       >
@@ -139,7 +140,8 @@ const CongratulatoryMoney = () => {
         onCancel={() => setBrideVisible(false)}
         footer={[
           <Description>
-            계좌번호 클릭시, 붙여넣기 가능한 텍스트로 복사됩니다.
+            계좌번호 클릭시,\n
+            붙여넣기 가능한 텍스트로 복사됩니다.
           </Description>,
         ]}
       >
